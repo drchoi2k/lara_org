75e47d803dab404ad6eacd5feb7978a26801d282
CHOI.DR <drchoi2k@naver.com>
2017-08-22 08:56:57 +0900

    welcomecontroller추가

---
+++ app/Http/Controllers/welcomecontroller.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class welcomecontroller extends Controller
+{
+    /**
+     * Say hello to visitors.
+     *
+     * @return \Illuminate\Contracts\View\Factory
+     */
+    public function index() {
+        return view('welcome');
+    }
+
+    /**
+     * Set locale.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function locale()
+    {
+        $cookie = cookie()->forever('locale__myapp', request('locale'));
+
+        cookie()->queue($cookie);
+
+        return ($return = request('return'))
+            ? redirect(urldecode($return))->withCookie($cookie)
+            : redirect('/')->withCookie($cookie);
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<link type="text/css" rel="stylesheet" href="/resource/view/style.css" /> 
+  <head>
+    <meta charset="utf-8">
+    <title>아마존 EC2</title>
+  </head>
+  <body>
+    <div class="title">
+      <a>여기는 아마존 서버입니다. </a>
+    </div>
+  </body>
+</html>
