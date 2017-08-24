86f3f315d76f4a1812afbf142dbf1a4d2d55a020
drchoi2k <drchoi2k@naver.com>
2017-08-24 13:05:57 +0900

    2017.08.24 작업분

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<style type="text/css">
+ 	.title{
+ 		text-align: center;
+  		margin: 80px 0 0 0;
+  		color: #2e6b6f;
+  		font-size: 50pt;
+  		font-weight: 700;
+  	}
+</style>
+	<head>
+		<title>공사중</title>
+	</head>
+	<body>
+		<div class="title">
+		<hr>
+		<img src="images/gong.png">
+				<!-- <tr>공사중<br>데이타베이스 업그레이드 중!</tr> -->
+		<hr>
+		</div>
+	</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,65 +5,11 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
+        <title>라 라 벨</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #333;
-                color: #fff;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 110;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 85px;
-            }
-
-            .links > a {
-                color: #fff;
-                padding: 0 25px;
-                font-size: 14px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
+        <link href="css/welcome.css" rel="stylesheet" type="text/css">
+     </head>
     <body>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
