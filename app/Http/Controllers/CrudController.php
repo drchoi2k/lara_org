4a4a75c5a02ef08e6579c4095a6ca26f10c7cfb6
drchoi2k <drchoi2k@naver.com>
2017-08-30 02:10:13 +0900

    no message

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests;
+// use App\Http\Requests;
 use App\Role;
 use App\User;
-use Illuminate\Support\Facades\Input;
-use Illuminate\Support\Facades\Redirect;
+// use Illuminate\Support\Facades\Input;
+// use Illuminate\Support\Facades\Redirect;
 class CrudController extends Controller
 {
     public function getInsert()
